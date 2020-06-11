95dcb2d8651645a55c05865145a9fbbcceea99cb
VitaliyKorolev <korolevva88@yandex.ru>
2020-06-11 22:16:06 +0300

    Дабавляет использование хуков useEffect и useState

--- src/components/welcom-screen/welcome-screen.jsx
+++ src/components/welcom-screen/welcome-screen.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const WelcomeScreen = (props) => {
   // eslint-disable-next-line react/prop-types
   const {errorsCount} = props;
+
+  const [errors, setErrorsCount] = useState(errorsCount);
+
+  useEffect(() => {
+    document.title = `Можно допустить ${errors} ошибки`;
+  });
+
   return (
     <section className="welcome">
       <div className="welcome__logo">
         <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
       </div>
-      <button className="welcome__button">
+      <button className="welcome__button" onClick={() => setErrorsCount(errors + 1)}>
         <span className="visually-hidden">Начать игру</span>
       </button>
       <h2 className="welcome__rules-title">Правила игры</h2>
       <p className="welcome__text">Правила просты:</p>
       <ul className="welcome__rules-list">
         <li>Нужно ответить на все вопросы.</li>
-        <li>Можно допустить {errorsCount} ошибки.</li>
+        <li>Можно допустить {errors} ошибки.</li>
       </ul>
       <p className="welcome__text">Удачи!</p>
     </section>
