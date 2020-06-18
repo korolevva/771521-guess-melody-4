08d72068ad821b8dc937f36e64753010960e7154
VitaliyKorolev <korolevva88@yandex.ru>
2020-06-18 22:29:34 +0300

    Добавляет в компонент вопроса по жанру состояние

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -5,10 +5,14 @@ import {GameType} from "../../const.js";
 class GenreQuestionScreen extends PureComponent {
   constructor(props) {
     super(props);
+    this.state = {
+      answers: [false, false, false, false],
+    };
   }
 
   render() {
     const {onAnswer, question} = this.props;
+    const {answers: userAnswers} = this.state;
     const {
       answers,
       genre,
