a892293feb3a9f174cf0a4fa879f97acf6a93e91
VitaliyKorolev <korolevva88@yandex.ru>
2020-06-18 22:36:16 +0300

    Опишет метод rendor  в компоненте по жанру

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -2,7 +2,7 @@ import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {GameType} from "../../const.js";
 
-class GenreQuestionScreen extends PureComponent {
+className GenreQuestionScreen extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
@@ -19,7 +19,30 @@ class GenreQuestionScreen extends PureComponent {
     } = question;
 
     return(
+      <section className="game game--genre">
+        <header className="game__header">
+          <a className="game__back" href="#">
+            <span className="visually-hidden">Сыграть ещё раз</span>
+            <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"/>
+          </a>
 
+          <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+            <circle className="timer__line" cx="390" cy="390" r="370"
+                    style="filter: url(#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"/>
+          </svg>
+
+          <div className="game__mistakes">
+            <div className="wrong"></div>
+            <div className="wrong"></div>
+            <div className="wrong"></div>
+          </div>
+        </header>
+
+        <section className="game__screen">
+          <h2 className="game__title">Выберите {genre} треки</h2>
+
+        </section>
+      </section>
     );
   }
 }
