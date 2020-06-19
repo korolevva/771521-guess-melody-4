c6e6f23d9d1ef73abcc9a7b8568be6685a937768
VitaliyKorolev <korolevva88@yandex.ru>
2020-06-19 14:32:10 +0300

    Описывает метод render компонента с вопросами по артистам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import WelcomeScreen from "../welcom-screen/welcome-screen.jsx";
 import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
 
 const welcomeButtonHandler = () => {};
 
@@ -16,9 +17,12 @@ const App = ({errorsCount, questions}) => {
             onWelcomeButtonClick={welcomeButtonHandler}
           />
         </Route>
-        {/* <Route exact path="/dev-artist">
-          <ArtistQuestionScreen />
-        </Route> */}
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen
+            question={questions[1]}
+            onAnswer={() => {}}
+          />
+        </Route>
         <Route exact path="/dev-genre">
           <GenreQuestionScreen
             question={questions[0]}
---
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {GameType} from "../../const.js";
+
+const ArtistQuestionScreen = ({onAnswer, question}) => {
+  return (
+    <section className="game game--artist">
+      <header className="game__header">
+        <a className="game__back" href="#">
+          <span className="visually-hidden">Сыграть ещё раз</span>
+          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"/>
+        </a>
+
+        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+          <circle className="timer__line" cx="390" cy="390" r="370" style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}} />
+        </svg>
+
+        <div className="game__mistakes">
+          <div className="wrong"></div>
+          <div className="wrong"></div>
+          <div className="wrong"></div>
+        </div>
+      </header>
+
+      <section className="game__screen">
+        <h2 className="game__title">Кто исполняет эту песню?</h2>
+        <div className="game__track">
+          <div className="track">
+            <button className="track__button track__button--play" type="button"></button>
+            <div className="track__status">
+              <audio></audio>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+ArtistQuestionScreen.propTypes = {
+  onAnswer: PropTypes.func.isRequired,
+  question: PropTypes.shape({
+    type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired,
+    song: PropTypes.shape({
+      artist: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired,
+    }).isRequired,
+    answers: PropTypes.arrayOf(PropTypes.shape({
+      picture: PropTypes.string.isRequired,
+      artist: PropTypes.string.isRequired,
+    })).isRequired,
+  })
+}
+
+export default ArtistQuestionScreen;
