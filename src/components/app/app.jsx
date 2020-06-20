70122f90382c88e563df8af3193714dfa65e2fd8
VitaliyKorolev <korolevva88@yandex.ru>
2020-06-21 00:02:40 +0300

    Превращает App из функционального в класс-компонент

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,38 +1,42 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import WelcomeScreen from "../welcom-screen/welcome-screen.jsx";
 import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
 import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
 
-const welcomeButtonHandler = () => {};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-const App = ({errorsCount, questions}) => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <WelcomeScreen
-            errorsCount={errorsCount}
-            onWelcomeButtonClick={welcomeButtonHandler}
-          />
-        </Route>
-        <Route exact path="/dev-artist">
-          <ArtistQuestionScreen
-            question={questions[1]}
-            onAnswer={() => {}}
-          />
-        </Route>
-        <Route exact path="/dev-genre">
-          <GenreQuestionScreen
-            question={questions[0]}
-            onAnswer={() => {}}
-          />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
-};
+  render() {
+    const {errorsCount, questions} = this.props;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <WelcomeScreen
+              errorsCount={errorsCount}
+            />
+          </Route>
+          <Route exact path="/dev-artist">
+            <ArtistQuestionScreen
+              question={questions[1]}
+              onAnswer={() => {}}
+            />
+          </Route>
+          <Route exact path="/dev-genre">
+            <GenreQuestionScreen
+              question={questions[0]}
+              onAnswer={() => {}}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
