83fda3c421638269a6f3f1661ba964780ee619be
VitaliyKorolev <korolevva88@yandex.ru>
2020-07-11 11:42:30 +0300

    Устанавливает redux, react-redux и подготавливает начальную инфраструктуру

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 import questions from "./mock/questions.js";
+import {createStore} from "redux";
+import Provider from "redux";
 
 const Settings = {
   ERRORS_COUNT: 3
 };
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <App
-      errorsCount={Settings.ERRORS_COUNT}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App
+        errorsCount={Settings.ERRORS_COUNT}
+        questions={questions}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
