a451bbbca21f192de9cb2248ce0c395a74fc261e
VitaliyKorolev <korolevva88@yandex.ru>
2020-07-12 19:49:38 +0300

    Добавляет проверку ответов, подключает моки для store, подключает компонент game-screen к store, добавит компонент Mistakes

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,19 @@
 import {extend} from "./utils.js";
+import {GameType} from "./const.js";
 
 const initialState = {
   mistakes: 0,
   step: -1,
+};
+
+const isArtistAnswerCorrect = (question, userAnswer) => {
+  return userAnswer.artist === question.song.artist;
+};
+
+const isGenreAnswerCorrect = (question, userAnswer) => {
+  return userAnswer.every((it, i) => {
+    return it === (question.answers[i].genre === question.genre);
+  });
 };
 
 const ActionType = {
@@ -14,7 +25,25 @@ const ActionCreator = {
   incrementStep: () => ({
     type: ActionType.INCREMENT_STEP,
     payload: 1,
-  })
+  }),
+
+  incrementMistake: (question, userAnswer) => {
+    let answerIsCorrect = false;
+
+    switch (question.type) {
+      case GameType.ARTIST:
+        answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
+        break;
+      case GameType.GENRE:
+        answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
+        break;
+    }
+
+    return {
+      type: ActionType.INCREMENT_MISTAKES,
+      payload: answerIsCorrect ? 0 : 1,
+    };
+  },
 };
 
 const reducer = (state = initialState, action) => {
